fd3ab0b228dc4a24c586562295224ae4a38a9bce
徳永龍仁 <ryujin-maru@tokunagaryuuhitoshinoMacBook-Air.local>
2023-06-18 14:34:07 +0900

    image

--- routes/owner.php
+++ routes/owner.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,6 +35,8 @@ Route::prefix('shops')->
         Route::get('edit/{shop}',[ShopController::class,'edit'])->name('shops.edit');
         Route::post('update/{shop}',[ShopController::class,'update'])->name('shops.update');
     });
+
+Route::resource('images',ImagesController::class)->except(['show'])->middleware('auth:owners');
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
